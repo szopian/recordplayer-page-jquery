a36b821960c5401c7853f449872c4ce8754c810a
Adri Ana <szopiangreg@gmail.com>
2021-03-05 17:14:35 +0000

    commit files

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  $(".slider").cycle({
+    fx: "fade",
+    delay: -2000,
+    prev: "#prev",
+    next: "#next",
+  });
+});
+
+// $(".slider").cycle({
+//   fx: "shuffle",
+//   easing: "easeOutBack",
+//   delay: -4000,
+// });
